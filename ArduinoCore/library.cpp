c68b670f25ae017a53f2b9123dbcff06fba0c242
aeon32 <aeon32@mail.ru>
2018-09-22 13:55:19 +0300

    Init commit

---
+++ ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 9/22/2018 12:55:52 PM
+ * Author : 3ema
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ pumping/Release/firewire_upload.bat
@@ -0,0 +1,1 @@
+C:\Programmer\avrdude\avrdude.exe  -v -pm2560 -cwiring -PCOM5 -b115200  -D -Uflash:w:GSM-WebClient.hex:i
