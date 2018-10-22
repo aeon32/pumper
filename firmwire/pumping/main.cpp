95c3f8c679882d02e6e83ebfc13673de5a550c91
aeon32 <aeon32@mail.ru>
2018-10-21 23:39:29 -0800

    Sending from SIM900A tested

--- firmwire/pumping/main.cpp
+++ firmwire/pumping/main.cpp
@@ -18,10 +18,15 @@
 
 #define PINPOWER 9
 #define DEBUG false
-#define APN "internet"
+#define APN "internet.tele2.ru"
+#define URL "http://aeration.000webhostapp.com/controlserver/" 
 #define USERNAME ""
 #define PASSWORD ""
 
+
+
+const char * imei = NULL;
+char fullUrl[256];
 //SoftwareSerial SSerial(10, 11);
 
 DTE dte(Serial1, PINPOWER, DEBUG);
@@ -37,9 +42,24 @@ void setup() {
   Serial.println("Power Reset\r\n");
   dte.powerReset();
   ip.setConnectionParamGprs(APN, USERNAME, PASSWORD);
+  
+  
+  while (!imei)
+  {
+	  imei = dte.getProductSerialNumberIdentification();
+	  if (!imei)
+	  {
+		  delay(5000);
+	  }
+	  
+  }
+  
+  strcpy(fullUrl,URL);
+  strcat(fullUrl, "?MGFiY2Q=");
 }
 
 void loop() {
+  bool ok;
   if (!dte.AT())
     dte.powerReset();
 
@@ -52,7 +72,10 @@ void loop() {
     Serial.println();
     unsigned long timeout = 30;  // The minimum HTTP Timeout setting
     http.initialize(timeout);    // First parameter is for enabling SSL
-    http.action("GET", "https://arduino.cc/asciilogo.txt");
+	Serial.println("Sending");
+	Serial.println(fullUrl);
+    ok = http.action("GET", fullUrl);
+	Serial.println(ok ? "sent" : "failed");
     unsigned long t = millis();
     while ((millis() - t < (timeout + 1) * 1000) && !Urc.httpAction.updated && dte.AT())
       dte.clearReceivedBuffer();  // This is necessary, so URC can be captured
@@ -87,6 +110,7 @@ void loop() {
     }
     http.terminate();
     Serial.println("Done!");
+	/*
     Serial.println("Press any key to see next connection behave...");
     Serial.println("*It's already connected, so it's faster. :)");
     while (Serial.available() == 0)
@@ -95,6 +119,7 @@ void loop() {
       Serial.read();
       delay(50);
     }
+	*/
   } else {
     Serial.print("Network Registration: ");
     Serial.println(gsm.getNetworkRegistration().status);
@@ -103,5 +128,5 @@ void loop() {
     Serial.println("Wait to connect...");
     Serial.println();
   }
-  delay(1000);  //It's just for debugging, no need to delay actually
+  delay(100);  //It's just for debugging, no need to delay actually
 }
