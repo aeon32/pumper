a94466c6b26fe93bd1f422caf66557ad8bc0c3b7
aeon32 <aeon32@mail.ru>
2019-03-27 18:02:29 +0300

    Some changes in js error reporting

--- web/components/com_controller_edit/com_controller_edit.php
+++ web/components/com_controller_edit/com_controller_edit.php
@@ -6,13 +6,10 @@ require_once(PATH_BASE . '/framework/utils.php');
 class com_controller_edit extends AbstractComponent
 {
     //варианты режима работы
-    const MODE_CREATE_NEW = 1;
-    const MODE_EDIT_NEW = 2;
-    const MODE_NOT_FOUND = 3;
-    const MODE_SHOW = 4;
-    const MODE_SAVE = 5;
-    const MODE_DELETE = 6;
-    const MODE_UPLOAD = 7;
+
+    const MODE_NOT_FOUND = 1;
+    const MODE_SHOW = 2;
+
 
     private $limit_random = 20;
     private $site = NULL;
@@ -28,43 +25,16 @@ class com_controller_edit extends AbstractComponent
         if (!$user->getAuthorized())
             die();
         $id = array_key_exists('id', $_GET) ? $_GET['id'] : NULL;
-        if ($site->getAction() == 'save') {
-            /*
-         $id = (int) $_POST["scheme_id"];
-            $name = processMagicQuotes($_POST['name']);
 
-      $this->scheme = new Scheme($id,$name);
-      $this->scheme = $this->scheme_manager->save($this->scheme);
-      if (isset($this->scheme)) {
-          $id = $this->scheme->id;
-      } else {
-          $id = null;
-      };
-      $this->mode = self::MODE_SAVE;
-      */
-        } else if ($site->getAction() == 'delete') {
+        $this->mode = self::MODE_NOT_FOUND;
 
-
-            $id = (int)$id;
-            $deleted = $this->scheme_manager->deleteScheme($id);
-            $this->mode = $deleted > 0 ? self::MODE_DELETE : self::MODE_NOT_FOUND;
-
-
-        } else if ($site->getAction() == 'new') {
-            $this->scheme = $this->scheme_manager->createTemporary();
-            $this->mode = self::MODE_CREATE_NEW;
-        } else if (is_numeric($id)) {
+        if (is_numeric($id)) {
             $id = (int)$id;
             $this->controller = $this->controller_manager->getController($id);
             if (!$this->controller) {
                 $this->mode = self::MODE_NOT_FOUND;
-            } else if ($this->scheme->temporary) {
-                $this->mode = self::MODE_EDIT_NEW;
             } else {
                 $this->mode = self::MODE_SHOW;
-            };
-            if ($this->mode != self::MODE_NOT_FOUND && $site->getAction() == 'upload_map') {
-                $this->mode = $this->controller_manager->uploadMap($id) ? self::MODE_UPLOAD : self::MODE_UPLOAD_ERROR;
             };
         } else {
             $this->mode = self::MODE_NOT_FOUND;
@@ -75,23 +45,11 @@ class com_controller_edit extends AbstractComponent
     public function getTitle()
     {
         switch ($this->mode) {
-            case self::MODE_CREATE_NEW:
-                return "Создание нового контроллера";
-                break;
-            case self::MODE_EDIT_NEW:
-                return "Создание схемы расположения точек wifi";
-                break;
             case self::MODE_NOT_FOUND:
                 return "Страница не найдена";
                 break;
             case self::MODE_SHOW:
                 return "Управление контроллером";
-                break;
-            case self::MODE_DELETE:
-                return "Страница удалена";
-                break;
-            case self::MODE_UPLOAD:
-                return "Схема размещения точек wifi";
                 break;
 
         };
@@ -125,15 +83,16 @@ class com_controller_edit extends AbstractComponent
     private function renderForm()
     {
         ?>
-        <?php if ($this->mode == self::MODE_SAVE)
-        print("<h4>Cхема сохранена</h4>");
+        <?php
+         /*if ($this->mode == self::MODE_SAVE)
+        print("<h4>Cхема сохранена</h4>"); */
         ?>
 
         <div class="content">
             <div class="header_div">
                 <h1>Управление контроллером</h1>
             </div>
-            <div id="error_message">Ошибка обновления информации</div>
+            <div id="error_message" style="display:none">Ошибка обновления информации</div>
 
             <table id="pumping_table" class="list_table" width="100%">
                 <tbody>
@@ -166,23 +125,11 @@ class com_controller_edit extends AbstractComponent
     public function render()
     {
         switch ($this->mode) {
-            case self::MODE_CREATE_NEW:
-                $this->renderForm();
-                break;
-            case self::MODE_EDIT_NEW:
-                $this->renderForm();
-                break;
             case self::MODE_NOT_FOUND:
                 $this->renderNotFound();
                 break;
             case self::MODE_SHOW:
                 $this->renderForm();
-                break;
-            case self::MODE_SAVE:
-                $this->renderForm();
-                break;
-            case self::MODE_DELETE:
-                $this->renderDelete();
                 break;
             default:
                 $this->renderForm();
@@ -195,11 +142,11 @@ class com_controller_edit extends AbstractComponent
     public function renderDelete()
     {
         ?>
-        <div id="tabl">
-            <h1>Редактирование схемы</h1>
-        </div>
-        <div>
-            <h2>Схема удалена</h2>
+        <div class="content">
+            <div class="header_div">
+             <h1>Управление контроллером</h1>
+            </div>
+         <div id="error_message">Страница не найдена</div>
         </div>
         <?php
     }
@@ -217,11 +164,11 @@ class com_controller_edit extends AbstractComponent
     public function renderNotFound()
     {
         ?>
-        <div id="tabl">
-            <h1>Редактирование схемы</h1>
-        </div>
-        <div>
-            <h2>Страница не найдена</h2>
+        <div class="content">
+            <div class="header_div">
+                <h1>Управление контроллером</h1>
+            </div>
+            <div id="error_message">Страница не найдена</div>
         </div>
         <?php
     }
